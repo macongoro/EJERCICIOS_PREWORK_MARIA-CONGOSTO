bef525202ce925257eb47dae3cd8abe5737c0e82
macongoro <m.congostor@gmail.com>
2024-01-17 16:21:47 +0100

    ejercicio 1

--- SQL/ejercicio1.sql
+++ SQL/ejercicio1.sql
@@ -9,3 +9,10 @@ email VARCHAR (255) NOT NULL
 /* 2 Insertar un nuevo cliente en la tabla "Clientes" con id=1, nombre="Juan" y email="juan@example.com". */
 INSERT INTO public.clientes(name,email)
 VALUES('Juan','juan@example.com')
+/* 3.Actualizar el email del cliente con id=1 a "juan@gmail.com".*/
+UPDATE public.clientes
+SET email = 'juan@gmail.com'
+WHERE id=1
+/* 4. Eliminar el cliente con id=1 de la tabla "Clientes".*/
+DELETE FROM public.clientes
+WHERE id=1
